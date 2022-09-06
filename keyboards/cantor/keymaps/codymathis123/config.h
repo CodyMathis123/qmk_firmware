f75558759022009db89157314aa4fcfccb9ab6e7
Cody Mathis <cody.mathis123@gmail.com>
2022-09-05 21:54:34 -0400

    add cantor

---
+++ keyboards/cantor/keymaps/codymathis123/config.h
@@ -0,0 +1,3 @@
+#define COMBO_COUNT 1
+#define CAPS_WORD_IDLE_TIMEOUT 5000  // Turn off Caps Word after 5 seconds.
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
---
+++ keyboards/cantor/keymaps/codymathis123/rules.mk
@@ -0,0 +1,15 @@
+SRC += features/autocorrection.c
+
+MOUSEKEY_ENABLE = yes # Mouse keys
+EXTRAKEY_ENABLE = yes # Audio control and System control
+AUTO_SHIFT_ENABLE = yes # Auto Shift
+COMBO_ENABLE = yes # Enable combos
+NKRO_ENABLE = yes
+CONSOLE_ENABLE = no
+CAPS_WORD_ENABLE = yes
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+OLED_ENABLE = no
+LTO_ENABLE      = no
+
+USER_NAME := codymathis123
